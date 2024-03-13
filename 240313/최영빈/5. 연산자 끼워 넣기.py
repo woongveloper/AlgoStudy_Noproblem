fee252f2260a49af70c6e31e2960963e0384903b
swybean <swybean@gmail.com>
2024-03-14 08:26:46 +0900

    3/14_update_최영빈

--- 240313/최영빈/5. 연산자 끼워 넣기.py
+++ 240313/최영빈/5. 연산자 끼워 넣기.py
@@ -1,26 +1,56 @@
+def dfs(start, num):
+    global max_num, min_num, plus, minus, gob, nanugi
+    
+    if start == N:
+        max_num = max(max_num, num)
+        min_num = min(min_num, num)
+        return
+    
+    if plus > 0 :
+        plus -= 1
+        now_num = num + num_list[start]
+        dfs(start+1, now_num)
+        plus += 1
 
+    if minus > 0 :
+        minus -= 1
+        now_num = num - num_list[start]
+        dfs(start+1, now_num)
+        minus += 1
+
+    if gob > 0 :
+        gob -= 1
+        now_num = num * num_list[start]
+        dfs(start+1, now_num)
+        gob += 1
+
+    if nanugi > 0:
+        if num < 0:
+            nanugi -= 1
+            now_num = -(abs(num) // num_list[start])
+            dfs(start + 1, now_num)
+            nanugi += 1
+        else:
+            nanugi -= 1
+            now_num = num // num_list[start]
+            dfs(start + 1, now_num)
+            nanugi += 1
 
 
 N = int(input())                                        # 수의 개수 N
 num_list = list(map(int, input().split()))              # 숫자들 입력
-plus, minus, gob, nanugi = map(int, input().split())    # 연산자 개수 입력
+plus, minus, gob, nanugi = list(map(int, input().split()))    # 연산자 개수 입력
 
-max_num = 987654321     # 최대값 설정
-min_num = -987654321    # 최소값 설정
+max_num = -1000000000   # 최대값 설정
+min_num = 1000000000    # 최소값 설정
 
-for i in num_list:
-    if plus > 0:
-        i + (i+1)
-    if minus > 0:
-        i - (i+1)
-    if gob > 0:
-        i * (i+1)
-    if nanugi > 0:
-        i // (i+1)
+dfs(1, num_list[0])
 
-          
+print(max_num)
+print(min_num)
 
 
+# 시간초과
 
 
 '''
